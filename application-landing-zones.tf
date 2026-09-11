ad0cd70fcc459e9bca1fc99bb01bf4964177eb1c
Seshagiri Rao KB <messagefromgiri@live.com>
2026-09-11 19:04:53 +0530

    Bind application landing zones to management provider

--- application-landing-zones.tf
+++ application-landing-zones.tf
@@ -3,8 +3,13 @@
 # Scaled-down single-subscription lab
 # ============================================================
 
+# ============================================================
 # PCI Application Landing Zone
+# ============================================================
+
 resource "azurerm_resource_group" "app_pci" {
+  provider = azurerm.management
+
   name     = "rg-app-pci"
   location = "centralindia"
 
@@ -16,21 +21,9 @@ resource "azurerm_resource_group" "app_pci" {
   }
 }
 
-# Standard Application Landing Zone
-resource "azurerm_resource_group" "app_standard" {
-  name     = "rg-app-standard"
-  location = "centralindia"
-
-  tags = {
-    environment    = "lab"
-    landing_zone   = "standard"
-    classification = "standard"
-    managed_by     = "terraform"
-  }
-}
-
-# PCI Application VNet
 resource "azurerm_virtual_network" "app_pci" {
+  provider = azurerm.management
+
   name                = "vnet-app-pci"
   location            = azurerm_resource_group.app_pci.location
   resource_group_name = azurerm_resource_group.app_pci.name
@@ -44,39 +37,18 @@ resource "azurerm_virtual_network" "app_pci" {
   }
 }
 
-# Standard Application VNet
-resource "azurerm_virtual_network" "app_standard" {
-  name                = "vnet-app-standard"
-  location            = azurerm_resource_group.app_standard.location
-  resource_group_name = azurerm_resource_group.app_standard.name
-  address_space       = ["10.30.0.0/16"]
-
-  tags = {
-    environment    = "lab"
-    landing_zone   = "standard"
-    classification = "standard"
-    managed_by     = "terraform"
-  }
-}
-
-# PCI workload subnet
 resource "azurerm_subnet" "app_pci_workload" {
+  provider = azurerm.management
+
   name                 = "snet-workload"
   resource_group_name  = azurerm_resource_group.app_pci.name
   virtual_network_name = azurerm_virtual_network.app_pci.name
   address_prefixes     = ["10.20.1.0/24"]
 }
 
-# Standard workload subnet
-resource "azurerm_subnet" "app_standard_workload" {
-  name                 = "snet-workload"
-  resource_group_name  = azurerm_resource_group.app_standard.name
-  virtual_network_name = azurerm_virtual_network.app_standard.name
-  address_prefixes     = ["10.30.1.0/24"]
-}
-
-# PCI NSG
 resource "azurerm_network_security_group" "app_pci" {
+  provider = azurerm.management
+
   name                = "nsg-app-pci-workload"
   location            = azurerm_resource_group.app_pci.location
   resource_group_name = azurerm_resource_group.app_pci.name
@@ -101,8 +73,59 @@ resource "azurerm_network_security_group" "app_pci" {
   }
 }
 
-# Standard NSG
+resource "azurerm_subnet_network_security_group_association" "app_pci" {
+  provider = azurerm.management
+
+  subnet_id                 = azurerm_subnet.app_pci_workload.id
+  network_security_group_id = azurerm_network_security_group.app_pci.id
+}
+
+# ============================================================
+# Standard Application Landing Zone
+# ============================================================
+
+resource "azurerm_resource_group" "app_standard" {
+  provider = azurerm.management
+
+  name     = "rg-app-standard"
+  location = "centralindia"
+
+  tags = {
+    environment    = "lab"
+    landing_zone   = "standard"
+    classification = "standard"
+    managed_by     = "terraform"
+  }
+}
+
+resource "azurerm_virtual_network" "app_standard" {
+  provider = azurerm.management
+
+  name                = "vnet-app-standard"
+  location            = azurerm_resource_group.app_standard.location
+  resource_group_name = azurerm_resource_group.app_standard.name
+  address_space       = ["10.30.0.0/16"]
+
+  tags = {
+    environment    = "lab"
+    landing_zone   = "standard"
+    classification = "standard"
+    managed_by     = "terraform"
+  }
+}
+
+resource "azurerm_subnet" "app_standard_workload" {
+  provider = azurerm.management
+
+  name                 = "snet-workload"
+  resource_group_name  = azurerm_resource_group.app_standard.name
+  virtual_network_name = azurerm_virtual_network.app_standard.name
+  address_prefixes     = ["10.30.1.0/24"]
+}
+
 resource "azurerm_network_security_group" "app_standard" {
+  provider = azurerm.management
+
   name                = "nsg-app-standard-workload"
   location            = azurerm_resource_group.app_standard.location
   resource_group_name = azurerm_resource_group.app_standard.name
@@ -115,14 +138,9 @@ resource "azurerm_network_security_group" "app_standard" {
   }
 }
 
-# Associate PCI NSG to PCI workload subnet
-resource "azurerm_subnet_network_security_group_association" "app_pci" {
-  subnet_id                 = azurerm_subnet.app_pci_workload.id
-  network_security_group_id = azurerm_network_security_group.app_pci.id
-}
-
-# Associate Standard NSG to Standard workload subnet
 resource "azurerm_subnet_network_security_group_association" "app_standard" {
+  provider = azurerm.management
+
   subnet_id                 = azurerm_subnet.app_standard_workload.id
   network_security_group_id = azurerm_network_security_group.app_standard.id
 }
